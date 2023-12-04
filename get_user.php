3d2abe2737835c9f8337e365959e98f1af47371b
Rajnandan Kushwaha <rajnandanusa@gmail.com>
2023-12-04 11:16:09 +0530

    Update get_user.php

--- get_user.php
+++ get_user.php
@@ -1,15 +1,12 @@
 <?php
-// Include 'connect.php' and any necessary logic
+// Include 'connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $userId = $_GET['id'];
 
-    // Fetch user details from the database based on $userId
-    // Assuming $userDetails contains the details of the user to be deleted
-
     // Output HTML content for the delete confirmation modal
-    echo '<p>Are you sure you want to delete the user with ID: ' . $userId . '?</p>';
+    echo '<p>Are you sure you want to delete the reminder with ID: ' . $userId . '?</p>';
     echo '<button type="button" class="btn btn-danger" id="confirmDelete" data-id="' . $userId . '">Delete</button>';
-    echo '<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>';
+    echo '<button type="button" style="margin-left:5px;" class="btn btn-secondary" data-dismiss="modal">Cancel</button>';
 }
 ?>
