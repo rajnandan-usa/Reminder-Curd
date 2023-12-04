e49818b2767a1366a35be2327279682b5a80c052
Rajnandan Kushwaha <rajnandanusa@gmail.com>
2023-12-04 11:20:14 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,12 +1,21 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('upload_max_filesize', "400M");
+ini_set('post_max_size', '400M');
+ini_set('max_input_time', 600);
+ini_set('max_execution_time', 600);
+date_default_timezone_set("Asia/Calcutta"); 
+
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'reminder';
+$database = 'crud_reminder';
 
 $conn = mysqli_connect($host, $username, $password, $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
